7f34733ec4cc4003bef0677b65cb6aa62ce3983f
Gorzas <joseda87@gmail.com>
2019-06-06 15:05:58 +0200

    Add ember-cli-addon-docs to nf-core

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,14 @@
-import EmberRouter from '@ember/routing/router';
+/* eslint func-names: 0 array-callback-return: 0 */
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
+const Router = AddonDocsRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
-Router.map(() => ({}));
+Router.map(function () {
+  docsRoute(this, function () { /* Your docs routes go here */ });
+});
 
 export default Router;
